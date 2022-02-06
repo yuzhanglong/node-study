ebeafc63c58d023a7da2c77ae14227d27f6ba9e5
yuzhanglong <loveyzl1123@gmail.com>
2022-02-06 15:00:02 +0800

    feat: add demo

---
+++ demos/unref-timeout.js
@@ -0,0 +1,13 @@
+console.log('hello world!');
+
+const timer1 = setTimeout(() => {
+    console.log('[timer 1] hello world!');
+}, 2000)
+
+
+const timer2 = setTimeout(() => {
+    console.log('[timer 2] hello world!');
+}, 5000)
+
+// timer2.unref();
+
